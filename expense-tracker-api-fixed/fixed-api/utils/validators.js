ce6d046e51248da8d62b47e9513335b6ee7ba325
prabhav266 <prabhav.2502@gmail.com>
2026-05-19 14:18:54 +0530

    Final Project Push Done

--- expense-tracker-api-fixed/fixed-api/utils/validators.js
+++
@@ -1,8 +0,0 @@
-const { z } = require("zod");
-
-exports.transactionSchema = z.object({
-    amount: z.number().positive(),
-    type: z.enum(["income","expense"]),
-    category: z.string().optional(),
-    note: z.string().optional()
-});
