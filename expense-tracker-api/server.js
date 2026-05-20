1ed4a825ca5bb8e872854816b19ba997aec10437
prabhav266 <prabhav.2502@gmail.com>
2026-05-20 19:16:00 +0530

    deployement changes

--- expense-tracker-api/server.js
+++ expense-tracker-api/server.js
@@ -2,24 +2,43 @@ const express = require("express");
 
 const cors = require("cors");
 
+require("dotenv").config();
+
+const authRoutes =
+  require("./routes/auth.routes");
+
+const transactionRoutes =
+  require("./routes/transaction.routes");
+
 const app = express();
 
-app.use(cors({
 
-  origin: [
-
-    "http://localhost:5173",
-
-    "https://spendwise-emvvyiqa7-prabhav266s-projects.vercel.app"
-
-  ],
-
-  credentials: true
-
-}));
+app.use(cors());
 
 app.use(express.json());
 
+
 app.use("/api/auth", authRoutes);
 
-app.use("/api/transactions", transactionRoutes);
+app.use(
+  "/api/transactions",
+  transactionRoutes
+);
+
+app.get("/", (req, res) => {
+
+  res.send("API is running");
+
+});
+
+
+const PORT =
+  process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+
+  console.log(
+    `Server running on port ${PORT}`
+  );
+
+});
